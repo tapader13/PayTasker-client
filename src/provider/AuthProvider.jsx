937c31bf1519bb98fb5f75932d4cf0199f18614b
tapader13 <minhajtapader0@gmail.com>
2025-01-14 12:58:29 -0500

    firebase setup and auth comp

---
+++ src/provider/AuthProvider.jsx
@@ -0,0 +1,72 @@
+import { createContext, useEffect, useState } from 'react';
+import { auth } from '../firebase/firebase.config';
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut,
+  GoogleAuthProvider,
+  signInWithPopup,
+  updateProfile,
+} from 'firebase/auth';
+import useAxiosPublic from '../hooks/useAxiosPublic';
+export const context = createContext(null);
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const axiosPublic = useAxiosPublic();
+  const signUpUser = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+  const loginUser = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+  const signInWithGoogle = () => {
+    const provider = new GoogleAuthProvider();
+    return signInWithPopup(auth, provider);
+  };
+  const logoutUser = () => {
+    return signOut(auth);
+  };
+  const updateProfileUser = (data) => {
+    return updateProfile(auth.currentUser, data);
+  };
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (usr) => {
+      setUser(usr);
+      if (usr) {
+        axiosPublic.post('/jwt', { email: usr?.email }).then((data) => {
+          // console.log(data, 4);
+          if (data?.data?.token) {
+            localStorage.setItem('access_token', data?.data?.token);
+            setLoading(false);
+          }
+        });
+      } else {
+        localStorage.removeItem('access_token');
+        setLoading(false);
+      }
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+  return (
+    <context.Provider
+      value={{
+        user,
+        setUser,
+        loading,
+        signUpUser,
+        loginUser,
+        logoutUser,
+        updateProfileUser,
+        signInWithGoogle,
+      }}
+    >
+      {children}
+    </context.Provider>
+  );
+};
+
+export default AuthProvider;
