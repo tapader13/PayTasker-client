61460e3b538c92da6c564573eab00dd1a336e76d
tapader13 <minhajtapader0@gmail.com>
2025-01-27 13:01:10 -0500

    design for sslcommerz

--- src/component/dashboard/CheckoutForm.jsx
+++ src/component/dashboard/CheckoutForm.jsx
@@ -136,7 +136,7 @@ const CheckoutForm = () => {
         }}
       />
       <button
-        className='bg-tertiary text-white px-4 py-2 mt-4'
+        className='bg-tertiary w-full text-white px-4 py-2 mt-4'
         type='submit'
         disabled={!stripe || !clientSecret}
       >
--- src/component/dashboard/Payment.jsx
+++ src/component/dashboard/Payment.jsx
@@ -1,19 +1,35 @@
 import { Elements } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
 import CheckoutForm from './CheckoutForm';
+import useAuth from '../../hooks/useAuth';
 const stripePromise = loadStripe(import.meta.env.VITE_STRIPE_PK);
+
 const Payment = () => {
+  const { user } = useAuth();
   return (
     <div className='max-w-xl space-y-20 mx-auto mt-10'>
       {' '}
       <div>
+        <h1 className='text-xl font-bold mb-5'>Payment Using Stripe</h1>
         <Elements stripe={stripePromise}>
           <CheckoutForm />
         </Elements>
       </div>
       <div>
-        <input className='w-full' type='email' name='email' id='' />
-        <button>Submit</button>
+        <h1 className='text-xl font-bold mb-5'>Payment Using SSLCOMMERZ</h1>
+        <div className='border p-4'>
+          <input
+            className='w-full p-2 border'
+            type='email'
+            name='email'
+            readOnly
+            defaultValue={user?.email}
+            id=''
+          />
+          <button className='w-full px-4 py-2 bg-tertiary text-white mt-4'>
+            Pay
+          </button>
+        </div>
       </div>
     </div>
   );
